e2eb64feb7e200d7ae8d4c60644d6a5f7ec322c3
Martin Laine <martin.laine@gmail.com>
2015-08-30 15:52:04 +0100

    Add package script

---
+++ scripts/package.js
@@ -0,0 +1,20 @@
+var packager = require("electron-packager")
+var path = require("path")
+
+packager({
+  dir: path.resolve(__dirname + "/.."),
+  out: path.resolve(__dirname + "/../dist/"),
+  name: "DownQuark",
+  platform: "linux",
+  arch: "x64",
+  version: "0.30.4",
+  ignore: [
+    "dist",
+    "node_modules/(electron-rebuild|\.bin)($|/)",
+    "node_modules/.*/(docs|test)($|/)"
+  ],
+  asar: true,
+  overwrite: true
+}, function (err, appPath) {
+  console.log("All done!")
+})
