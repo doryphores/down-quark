56e59916e3273e372c5c68d7e886e2c299c4777f
Martin Laine <martin.laine@gmail.com>
2015-08-30 12:24:32 +0100

    Stop compiling css in main process

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,6 @@
 import initWindow from "./utils/window"
-import compileCSS from "./utils/styles"
 import React from "react"
-import path from "path"
-import remote from "remote"
 import App from "./components/app"
 
-compileCSS(path.resolve(__dirname, "../static/css/app.styl"))
-  .then(function renderApp() {
-    initWindow()
-    React.render(<App />, document.getElementById("app"))
-  }).catch(function logError(err) {
-    remote.require("app").quit()
-    console.log(err.message)
-  })
+initWindow()
+React.render(<App />, document.getElementById("app"))
