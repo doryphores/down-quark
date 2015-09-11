e76bb9368d80c032ccf18613b3ec6eaef3614598
Martin Laine <martin.laine@simplybusiness.co.uk>
2015-09-11 15:08:45 +0100

    Better window bounds restore functionality

--- src/utils/window.js
+++ src/utils/window.js
@@ -1,31 +1,30 @@
 import remote from "remote"
 import LocalStorageManager from "../utils/local_storage_manager"
-
-var saveDims = function () {
-  LocalStorageManager.set("windowDimensions", {
-    x         : window.screenX,
-    y         : window.screenY,
-    width     : window.outerWidth,
-    height    : window.outerHeight,
-    maximized : remote.getCurrentWindow().isMaximized()
-  })
-}
+import _ from "underscore"
 
 export default function initWindow() {
-  window.addEventListener("unload", saveDims)
-  window.addEventListener("resize", saveDims)
+  const win = remote.getCurrentWindow()
+  const dims = LocalStorageManager.get("windowDimensions")
 
-  var dims = LocalStorageManager.get("windowDimensions")
+  const saveDims = _.debounce(() => {
+    LocalStorageManager.set("windowDimensions", {
+      bounds    : win.getBounds(),
+      maximized : win.isMaximized()
+    })
+  }, 100);
+
+  ["move", "resize"].forEach((event) => {
+    win.removeAllListeners(event)
+    win.on(event, saveDims)
+  })
 
   if (dims) {
-    if (dims.maximized === "true") {
-      remote.getCurrentWindow().show()
-      remote.getCurrentWindow().maximize()
-    } else {
-      window.resizeTo(dims.width, dims.height)
-      window.moveTo(dims.x, dims.y)
+    win.setBounds(dims.bounds)
+    win.show()
+
+    // Maximizing on OSX is a bit odd
+    if (process.platform != "darwin" && dims.maximized) {
+      win.maximize()
     }
   }
-
-  remote.getCurrentWindow().show()
 }
